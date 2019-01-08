3dbb8e48f7fb03fd9e7b990a059e4e8982ac66c2
james <jlbaker361@gmail.com>
2019-01-08 01:33:56 -0500

    generator still sucks

--- routes/marx.js
+++ routes/marx.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var spawn = require('child_process').spawn,
+    py    = spawn('python', ['python/basic.py']);
 
 router.get('/', function(req, res, next) {
 	res.render('marx', {content:'none'});
